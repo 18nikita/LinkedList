d9b17d1a432068e41919fd7d65392ee77e7dddf7
unknown <rajputnikita5666@gmail.com>
2021-10-12 20:56:28 +0530

    Added:value in between

--- src/main/java/com/bridgelabz/linkedlist/LinkedList.java
+++ src/main/java/com/bridgelabz/linkedlist/LinkedList.java
@@ -10,84 +10,146 @@ class Node {
 		this.data = data;
 		this.next = null;
 	}
+
+	@Override
+	public String toString() {
+		return "Node{" + "data=" + data + ", next=" + next;
+	}
 }
 
 class operations {
 	Node head;
 	Node tail;
 
-	public void pushData(int data) {		
-		Node newnode = new Node(data);
+	public Node pushData(int data) {
 
+		// created the object of the Node class to get the address of the Node and
+		// passing the "data" it will store in constructor
+		Node newnode = new Node(data);
 		if (head == null) {
 			head = newnode;
+			tail = newnode;
 		} else {
+			/*
+			 * swapping the value to get the new node address
+			 */
 			Node temp = head;
 			this.head = newnode;
 			newnode.next = temp;
+
 		}
+		return newnode;
+
 	}
 
 	public void print() {
 		if (head == null)
 			System.out.println("Empty LinkList");
+
 		else {
+
 			Node temp = head;
 			while (temp != null) {
 				System.out.print(temp.data + " -> ");
 				temp = temp.next;
 			}
+
 		}
+
 	}
 
 	public void append(int data) {
+
 		Node newnode = new Node(data);
 		if (head == null) {
 			head = newnode;
 			tail = newnode;
-		} else {
+		} 
+		else {
 			tail.next = newnode;
 			tail = newnode;
 		}
 	}
+
+	public void inBetweenInsert(Node previousNode, Node newNode) {
+		Node tempNode = previousNode.next;
+		previousNode.next = newNode;
+		newNode.next = tempNode;
+
+	}
+	
 }
 
-public class LinkedList {
+class LinkList {
+	static operations link = new operations();
 
 	public static void addDataStart() {
-		operations link = new operations();
 		link.pushData(70);
 		link.pushData(30);
 		link.pushData(56);
 		link.print();
 	}
 
-	public static void addDataEnd() {
-		operations link = new operations();
+	public void addDataEnd() {
+
 		link.append(56);
 		link.append(30);
 		link.append(70);
 		link.print();
 	}
 
+	public void insertInBetewwn() {
+
+		Node secondnode = link.pushData(70);
+
+		Node firstnode = link.pushData(56);
+		Node newnode = new Node(30);
+
+		System.out.println("Before:");
+		link.print();
+		link.inBetweenInsert(firstnode, newnode);
+		System.out.println("\nAfter:");
+		link.print();
+
+	}
+
+}
+
+public class LinkedList {
 	public static void main(String[] args) {
+
+		LinkList obj = new LinkList();
 
 		System.out.println("*** Welcome To LinkList Program ***");
 		Scanner sc = new Scanner(System.in);
 		System.out.println();
 		System.out.println("Press 1 to add data at start");
 		System.out.println("Press 2 to add data at end");
+		System.out.println("Press 3 to insert data in between");
+
 		int input = sc.nextInt();
 
 		switch (input) {
+
 		case 1:
-			addDataStart();
+			obj.addDataStart();
 			break;
+
 		case 2:
-			addDataEnd();
+
+			obj.addDataEnd();
 			break;
+
+		case 3:
+
+			obj.insertInBetewwn();
+			break;
+
 		default:
 			System.out.println("Invalid Choice");
+
 		}
+
 	}
+
 }
